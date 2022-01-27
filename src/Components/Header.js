ff976f31bee30d858fb025672c85fb3b6c9f994c
Aishwarya1202 <74051266+Aishwarya1202@users.noreply.github.com>
2022-01-28 02:46:57 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import { signOut } from "@firebase/auth";
+//import { signOut } from "@firebase/auth";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../Utils/AuthContext";
@@ -9,11 +9,11 @@ export default function Header() {
   const {data} = useData();
   return (
   <header>
-    <h1><Link to="/"> Bikezilla</Link></h1>
+    <h1><Link to="/"> Food Frenzy</Link></h1>
     <nav>
-      <Link to="/products">Products</Link>
+      <Link to="/products">Meals</Link>
       <Link to="/wishlist">Wishlist</Link>
-      <Link to="/cart">Cart {(user && data.cart && data.cart.quantity)?`(${data.cart.quantity})`:"(0)"} </Link>
+      <Link to="/cart">Orders {(user && data.cart && data.cart.quantity)?`(${data.cart.quantity})`:"(0)"} </Link>
       {(user)?   <Link to="/profile">Profile</Link>:    <Link to="/login">Login</Link>}
       {(user)?<button className="standalone" onClick={logout}> Logout </button>: <Link to="/signup">Signup</Link>}
     </nav>
