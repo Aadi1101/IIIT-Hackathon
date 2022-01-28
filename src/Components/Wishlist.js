4c9b15e44cdf5c3d0e419662a539d68e37d75971
Aishwarya1202 <74051266+Aishwarya1202@users.noreply.github.com>
2022-01-28 21:39:00 +0530

    Update Wishlist.js

--- src/Components/Wishlist.js
+++ src/Components/Wishlist.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useData } from "../Utils/DataContext";
+import StripeCheckout from "react-stripe-checkout";
 
 export default function Wishlist() {
   const { data, removeFromWishlist, clearWishlist } = useData();
+  const publishableKey ="pk_test_51JdeY8SIty0bliO7n0EUmMgT3QFpuBUC40HFrd1kFZWOA5UNM7GuX5hQms97Y0W6iDUJJFnp1iy2j00u2HlR2sjz00uNrHkStz";
 
   const onRemove = (id) => () => {
     removeFromWishlist(id);
@@ -14,17 +16,23 @@ export default function Wishlist() {
           <table>
             {Object.keys(data.wishlist).map((id) => (
               <tr>
-                <td>
-                  <img src={data.wishlist[id].img} />
-                  {data.wishlist[id].name}
-                </td>
-                <td>Buy @ ₹{data.wishlist[id].buyPrice}</td>
-                <td>Rent @ ₹{data.wishlist[id].rentPrice}/month</td>
-                <td>
-                  <button onClick={onRemove(id)}>Remove</button>
-                </td>
-              </tr>
+                  <td>
+                    {data.wishlist[id].name}
+                  </td>
+                  <td>Booked a table for 4</td>
+                  <td>Book @ ₹100</td>
+                  <td>{data.wishlist[id].quantity}</td>
+                  <td>
+                    <button onClick={onRemove(id)}>Remove</button>
+                  </td>
+                </tr>
+              
             ))}
+            <tr>
+              <th> Total </th>
+              <th> {(data.wishlist.quantity)*100} </th>
+              <th> - </th>
+            </tr>
           </table>
 
           <button className="standalone red" onClick={clearWishlist}>Clear Wishlist</button>
